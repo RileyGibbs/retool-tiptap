78d7f2631308efa720ab32dc32eb5b2ee6f813c4
Riley Gibbs <rjwgibbs@gmail.com>
2022-04-26 14:10:24 -0700

    Add tiptap to Retool component and replace example component

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ExampleComponent from "./ExampleComponent";
+import { default as RetoolEditor } from "./retool-editor";
 
-const RetoolConnectedComponent = Retool.connectReactComponent(ExampleComponent);
+const RetoolConnectedComponent = Retool.connectReactComponent(RetoolEditor);
 document.body.setAttribute('style', 'margin: 0;') 
 ReactDOM.render(
   <RetoolConnectedComponent/>, 
---
+++ src/retool-editor.js
@@ -0,0 +1,203 @@
+import React, { useEffect, useMemo } from 'react';
+import { useEditor, EditorContent } from '@tiptap/react';
+import StarterKit from '@tiptap/starter-kit';
+import './styles.scss';
+
+import CharacterCount from "@tiptap/extension-character-count";
+import Highlight from "@tiptap/extension-highlight";
+import Link from "@tiptap/extension-link";
+import Placeholder from "@tiptap/extension-placeholder";
+import Typography from "@tiptap/extension-typography";
+
+
+export function getExtensions(
+  { autoLink = true, maxLength, placeholder } = {},
+  customExtensions = [],
+) {
+  return [
+    Highlight,
+    Typography,
+    StarterKit,
+    // Emoji.configure({
+    //   emojis,
+    //   suggestion,
+    // }),
+    autoLink ? Link : Link.configure({ autolink: false }),
+    maxLength && CharacterCount,
+    placeholder && Placeholder.configure({ placeholder }),
+    ...customExtensions,
+  ].filter(Boolean);
+}
+
+export function useExtensions(
+  { autoLink = true, maxLength, placeholder } = {},
+  customExtensions,
+) {
+  return useMemo(() => getExtensions({ autoLink, maxLength, placeholder }, customExtensions), [
+    autoLink,
+    customExtensions,
+    maxLength,
+    placeholder,
+  ]);
+}
+
+
+const DEFAULT_VALUE = {
+  type: "doc",
+  content: [],
+};
+
+const MenuBar = ({ editor }) => {
+  if (!editor) {
+    return null
+  }
+
+  return (
+    <>
+      <button
+        onClick={() => editor.chain().focus().toggleBold().run()}
+        className={editor.isActive('bold') ? 'is-active' : ''}
+      >
+        bold
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleItalic().run()}
+        className={editor.isActive('italic') ? 'is-active' : ''}
+      >
+        italic
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleStrike().run()}
+        className={editor.isActive('strike') ? 'is-active' : ''}
+      >
+        strike
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCode().run()}
+        className={editor.isActive('code') ? 'is-active' : ''}
+      >
+        code
+      </button>
+      <button onClick={() => editor.chain().focus().unsetAllMarks().run()}>
+        clear marks
+      </button>
+      <button onClick={() => editor.chain().focus().clearNodes().run()}>
+        clear nodes
+      </button>
+      <button
+        onClick={() => editor.chain().focus().setParagraph().run()}
+        className={editor.isActive('paragraph') ? 'is-active' : ''}
+      >
+        paragraph
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 1 }).run()}
+        className={editor.isActive('heading', { level: 1 }) ? 'is-active' : ''}
+      >
+        h1
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 2 }).run()}
+        className={editor.isActive('heading', { level: 2 }) ? 'is-active' : ''}
+      >
+        h2
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 3 }).run()}
+        className={editor.isActive('heading', { level: 3 }) ? 'is-active' : ''}
+      >
+        h3
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 4 }).run()}
+        className={editor.isActive('heading', { level: 4 }) ? 'is-active' : ''}
+      >
+        h4
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 5 }).run()}
+        className={editor.isActive('heading', { level: 5 }) ? 'is-active' : ''}
+      >
+        h5
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleHeading({ level: 6 }).run()}
+        className={editor.isActive('heading', { level: 6 }) ? 'is-active' : ''}
+      >
+        h6
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBulletList().run()}
+        className={editor.isActive('bulletList') ? 'is-active' : ''}
+      >
+        bullet list
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleOrderedList().run()}
+        className={editor.isActive('orderedList') ? 'is-active' : ''}
+      >
+        ordered list
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleCodeBlock().run()}
+        className={editor.isActive('codeBlock') ? 'is-active' : ''}
+      >
+        code block
+      </button>
+      <button
+        onClick={() => editor.chain().focus().toggleBlockquote().run()}
+        className={editor.isActive('blockquote') ? 'is-active' : ''}
+      >
+        blockquote
+      </button>
+      <button onClick={() => editor.chain().focus().setHorizontalRule().run()}>
+        horizontal rule
+      </button>
+      <button onClick={() => editor.chain().focus().setHardBreak().run()}>
+        hard break
+      </button>
+      <button onClick={() => editor.chain().focus().undo().run()}>
+        undo
+      </button>
+      <button onClick={() => editor.chain().focus().redo().run()}>
+        redo
+      </button>
+    </>
+  )
+}
+
+const RetoolEditor = ({ triggerQuery, model, modelUpdate }) => {
+  const value = model?.description ?? DEFAULT_VALUE;
+  const editor = useEditor({
+    extensions: useExtensions({ autoLink: false, maxLength: 2000, placeholder: "Description" }),
+    content: value,
+    onUpdate: (event) => {
+      modelUpdate({
+        description: event.editor.getJSON(),
+      });
+    },
+  });
+
+  useEffect(() => {
+    if (value != null) {
+      const commands = editor?.chain();
+      const selection = editor?.view.state.selection.$anchor.pos;
+
+      if (commands) {
+        commands.setContent(value ?? DEFAULT_VALUE);
+        if (selection) commands.setTextSelection(selection);
+
+        commands.run();
+      }
+    }
+  }, [value, editor]);
+
+  return (
+    <div>
+      <MenuBar editor={editor} />
+      <EditorContent editor={editor} />
+    </div>
+  )
+}
+
+export default RetoolEditor;
